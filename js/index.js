e1bd22411a1c62504bc0a7b05acb42a8e6d3d183
Hong Baojin <13809735212@163.com>
2019-04-23 16:49:25 +0800

    lunbotu qusetion

--- js/index.js
+++ js/index.js
@@ -15,11 +15,56 @@ $(function () {
   $(".llp-step__item").each(function (index, e) {
     $(e).mouseover(function () {
       // console.log(111111111111)
-      $(this).addClass("six-circle__item--active").siblings().removeClass("six-circle__item--active")
+      $(this).addClass("six-circle__item--active").siblings().removeClass("six-circle__item--active");
+      $($(".llp-step-content__item")[index]).show(800).siblings().hide(800);
     })
-    // $(e).mouseenter(function () {
-    //   $(this).addClass(".six-circle__item--active").siblings().removeClass(".six-circle__item--active")
-    // })
   })
 
+  // 轮次带学
+  $(".method__tab").each(function (index, e) {
+    $(e).click(function () {
+      $(this).addClass("method__tab--active").siblings().removeClass("method__tab--active");
+      $($(".method__contents .jp-content")[index]).css("display", "block").addClass("method__content")
+          .siblings().removeClass("method__content").css("display", "none");
+    })
+  })
+
+  // 资深师资
+  $("#jp-teacher-names li").each(function (index, e) {
+    $(e).click(function () {
+      $(this).addClass("active").siblings().removeClass("active");
+    })
+  })
+
+  // var swiper = new Swiper('.swiper-container', {
+  //   slidesPerView: 3,
+  //   // effect : 'coverflow',
+  //   // spaceBetween: 30,
+  //   centeredSlides: true,
+  //
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //     type: 'custom'
+  //   }
+  // });
+
+  // 新东方大牛老师考前神预测
+  $(".daniu-predict__tab").each(function (index, e) {
+    $(e).click(function () {
+      $(this).addClass("daniu-predict__tab--active").siblings().removeClass("daniu-predict__tab--active");
+      $($(".daniu-predict__contents .jp-content")[index]).css("display", "block").addClass("daniu-predict__content")
+          .siblings().removeClass("daniu-predict__content").css("display", "none");
+    })
+  })
+
+  var footerWidth = $(".jp-footer-wrap").width();
+
+  var footerMargin = window.Number(35);
+
+  var time = 1;
+  $(".jp-footer-next").click(function () {
+    console.log($("#jp-list").css("left", -(footerWidth + footerMargin) * time));
+    time++
+  })
 })
